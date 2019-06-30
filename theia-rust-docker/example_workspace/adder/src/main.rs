02ad03ff7679c5417ce173baa05e8a293bc1a05c
Dinne Bosman <dwjbosman@yahoo.com>
2019-06-30 22:52:52 +0200

    manual gdb-rust check
    Signed-off-by: Dinne Bosman <dwjbosman@yahoo.com>

--- theia-rust-docker/example_workspace/adder/src/main.rs
+++ theia-rust-docker/example_workspace/adder/src/main.rs
@@ -1,14 +1,17 @@
-// This is a comment, and will be ignored by the compiler
-// You can test this code by clicking the "Run" button over there ->
-// or if prefer to use your keyboard, you can use the "Ctrl + Enter" shortcut
-
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
-
-// This is the main function
+// pretty.rs
+const COLORS: [&'static str;7] =
+  ["red", "yellow", "pink", "green", "purple", "orange", "blue"];
+struct Label {
+    index: usize,
+    color: &'static str
+}
 fn main() {
-    // The statements here will be executed when the compiled binary is called
-
-    // Print text to the console
-    println!("Hello World!");
+    // create a Vec of Label
+    let labels: Vec<Label> = (0..10).map(|i| {
+        Label { index: i, color: COLORS[i % COLORS.len()] }
+      }).collect();
+    // print them
+    for label in labels {
+        println!("{}: {}", label.index, label.color);
+    }
 }
